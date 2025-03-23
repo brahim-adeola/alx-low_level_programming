c120107802a5368ce32858f65218df662f41ed3b
brahim-adeola <brahimadeola2020@gmail.com>
2025-03-23 10:26:47 +0000

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - Compute the absolute value of an integer
+ * @n: Integer value
+ *
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
