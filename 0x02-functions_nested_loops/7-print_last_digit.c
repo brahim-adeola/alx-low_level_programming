a97c2517afb07ee4f4488110402daf4ac6ed8634
adeolaxdeus <adeolaxdeus@gmail.com>
2025-05-13 03:48:55 -0700

    fix func to handle int_min

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,9 @@ int print_last_digit(int num)
 {
 	int last_digit;
 
-	if (num < 0)
-		num = num * -1;
 	last_digit = num % 10;
+	if (last_digit < 0)
+		last_digit = last_digit * -1;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
