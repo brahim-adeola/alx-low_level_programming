84e9a2446d24573c2fedb212b20adcf5f5298696
adeolaxdeus <adeolaxdeus@gmail.com>
2025-05-13 03:19:54 -0700

    create a mechanism to handle negative params

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,11 @@
  */
 int print_last_digit(int num)
 {
-	int last_digit = num % 10;
+	int last_digit;
 
+	if (num < 0)
+		num = num * -1;
+	last_digit = num % 10;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
